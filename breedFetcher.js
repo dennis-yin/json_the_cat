70d7a5fd47af29039e5ca9e0abb3b80ed7d6c506
dennis-yin <dennis.yin26@gmail.com>
2019-10-22 00:50:05 +0000

    testing breedFetcher with Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,28 @@
 const request = require('request');
-const arg = process.argv[2];
 
-request("https://api.thecatapi.com/v1/breeds/", function(error, response, body) {
-  if (error) {
-    console.log("Error: Invalid URL");
-  } else {
-
-    const data = JSON.parse(body);
-    
-    let found = false;
-    for (const breed in data) {
-      if (data[breed]['name'] === arg) {
-        console.log(data[breed]['description']);
-        found = true;
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/", function(error, response, body) {
+    if (error) {
+      callback("Couldn't reach API");
+    } else {
+      const data = JSON.parse(body);
+      let description = "";
+      
+      let found = false;
+      for (const breed in data) {
+        if (data[breed]['name'] === breedName) {
+          description = data[breed]['description'];
+          found = true;
+        }
       }
+
+      if (!found) {
+        callback("Breed not found");
+      }
+
+      callback(null, description);
     }
+  });
+};
 
-    if (!found) {
-      console.log("Error: Breed not found");
-    }
-
-  }
-});
-
+module.exports = { fetchBreedDescription };
